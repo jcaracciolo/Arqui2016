61a4c5765b43a2ad7be7c3b44d916e6e6f908566
jcaracciolo <jcaracciolo@itba.edu.ar>
2016-06-15 00:38:14 -0300

    Fixed video, fractal working (not really)

--- Kernel/handlers.c
+++ Kernel/handlers.c
@@ -33,9 +33,6 @@ void irqDispatcher(int irq){
 		case 1:
 			addToBuffer();
             break;
-		case 8:
-			printNum(7,5);
-			break;
     }
 
 	return;
@@ -58,7 +55,6 @@ void setup_IDT()
 {
 	setup_IDT_entry (0x20, 0x08, (qword)&_irq00Handler, ACS_INT);
 	setup_IDT_entry (0x21, 0x08, (qword)&_irq01Handler, ACS_INT);
-	setup_IDT_entry (0x28, 0x08, (qword)&_irq05Handler, ACS_INT);
 
 	//idtr.base = 0;
 	//idtr.base +=(dword) &idt;
